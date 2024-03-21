c7b6613e64e425f8d70149c80620be84a0186e3b
HinohArata <mmgcntk@gmail.com>
2024-03-21 14:49:00 +0000

    surya: vendorsetup: Add important repositories

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+# Clone Vendor Source
+git clone https://github.com/ProjectEverest-Devices/android_vendor_xiaomi_surya kernel/xiaomi/surya
+
+# Clone Kernel Source
+git clone https://github.com/ProjectEverest-Devices/android_kernel_xiaomi_surya.git vendor/xiaomi/surya
+
+# Clone Vendor Leica/Miui Camera
+git clone https://gitlab.com/HinohArata/vendor_xiaomi_surya-miuicamera.git -b new vendor/xiaomi/surya-miuicamera
+
+# Clone Device Leica/Miui Camera
+git clone https://gitlab.com/HinohArata/device_xiaomi_surya-miuicamera.git device/xiaomi/surya-miuicamera
+
+# Clone Firmware Images
+git clone https://gitlab.com/HinohArata/firmware_xiaomi_surya.git firmware/xiaomi/surya
+
